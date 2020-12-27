77494b09a850d368d9d3443a1dea32470362a6a8
jasminecao <jasminecao03@gmail.com>
2020-12-27 13:28:35 -0500

    delete

--- login.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('form.login').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  let username = document.getElementById('username');
-  let password = document.getElementById('password');
-
-  console.log(username.value);
-  console.log(password.value);
-});
